a9152e3033906099a8fa32b9e24304f6d05e10ee
Thomas Henderson <tbhenderson55@yahoo.com>
2019-07-14 18:22:13 +0900

    cleaned up the cards a bit

--- src/components/ApartmentCard.js
+++ src/components/ApartmentCard.js
@@ -6,8 +6,13 @@ const ApartmentCard = () => {
   const [{ apartments }, dispatch] = useStateValue();
 
   const Apartment = styled.div`
-    max-width: 45%;
-    min-width: 300px;
+    width: 500px;
+    &:hover {
+      border-color: #3e3f3a;
+    }
+    @media (max-width: 990px) { 
+      width: 400px;
+     }
   `;
   const ApartmentImage = styled.img`
     height: 270px;
@@ -30,24 +35,18 @@ const ApartmentCard = () => {
         <Apartment className="card mb-3">
           <h3 className="card-header">{apartment.name}</h3>
           <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <h6 className="card-subtitle text-muted">Support card subtitle</h6>
+            <h5 className="card-title">{apartment.description}</h5>
+            <h6 className="card-subtitle text-muted">{apartment.price}</h6>
           </div>
           <ApartmentImage src={apartment.mainImage} alt="Card image"/>
           <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p className="card-text">{apartment.amenities}</p>
           </div>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item">Cras justo odio</li>
-            <li className="list-group-item">Dapibus ac facilisis in</li>
-            <li className="list-group-item">Vestibulum at eros</li>
+            <li className="list-group-item">{apartment.amenities}</li>
           </ul>
           <div className="card-body">
-            <a href="#" className="card-link">Card link</a>
-            <a href="#" className="card-link">Another link</a>
-          </div>
-          <div className="card-footer text-muted">
-            2 days ago
+            <a href="contact" className="card-link">Inquire here</a>
           </div>
         </Apartment>
       )
--- src/data/apartments.js
+++ src/data/apartments.js
@@ -2,17 +2,17 @@ export const apartments = [
     {
       "name": "Comfortable in Shinjuku",
       "description": "Close to all the excitement of Kabukicho!",
-      "description-long": "",
+      "descriptionLong": "",
       "mainImage": "https://ik.imagekit.io/toto55/9mins1_I74zYS-Rf.png",
-      "price": "130,000 JPY per month",
-      "amenities": "Double bed, Small Kitchen, TV, Sofa"
+      "price": "130,000 JPY per month (23.4 sqm)",
+      "amenities": "Double bed, Small Kitchen, TV, Sofa, All Bills inc., Portable Wi-Fi"
     },
     {
       "name": "9 mins to Shinjuku",
-      "description": "Next to the amazing Shinjku Gyoen park!",
-      "description-long": "",
+      "description": "Next to the amazing Shinjuku Gyoen park!",
+      "descriptionLong": "",
       "mainImage": "https://ik.imagekit.io/toto55/aptOne1_QPUOVnZ77.png",
-      "price": "140,000 JPY per month",
-      "amenities": "Double bed, Small Kitchen, TV, Sofa, Wshing Machine"
+      "price": "140,000 JPY per month (27.8 sqm)",
+      "amenities": "Double bed, Small Kitchen, TV, Sofa, Washing Machine, All Bills inc., High-Speed Unlimited Wi-Fi"
     }
   ]
