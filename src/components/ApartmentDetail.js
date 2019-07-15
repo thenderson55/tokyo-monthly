1d13e242c00b12ff047aa4ff6d1ae2360e50fbb2
Thomas Henderson <tbhenderson55@yahoo.com>
2019-07-15 15:58:00 +0900

    email added

--- src/components/ApartmentDetail.js
+++ src/components/ApartmentDetail.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useStateValue } from "../context/store";
 import styled from 'styled-components'
 import AliceCarousel from 'react-alice-carousel'
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,15 @@ import styled from 'styled-components'
 
 function Contact() {
 
+  const ContactText = styled.h3`
+    text-align:center;
+    margin: 30px;
+  `;
+
   return (
-    <div>
-      Hello
-    </div>
+    <ContactText>
+      For all inquires contact: tombonaventure@protonmail.com
+    </ContactText>
   );
 }
 
