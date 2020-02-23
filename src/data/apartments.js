fbe98b7554b7d10d23b79257982742e87c0cf024
Thomas Henderson <tbhenderson55@yahoo.com>
2020-02-24 08:51:10 +0900

    removed lisiting

--- src/data/apartments.js
+++ src/data/apartments.js
@@ -5,21 +5,10 @@ export const apartments = [
       "descriptionLong": "",
       "mainImage": "https://ik.imagekit.io/toto55/9mins1_I74zYS-Rf.png",
       "images": ["https://ik.imagekit.io/toto55/9mins1_I74zYS-Rf.png", "https://ik.imagekit.io/toto55/9mins4_Ieh00l7DS.png", "https://ik.imagekit.io/toto55/9mins2_NIJpdN8fV.png", "https://ik.imagekit.io/toto55/comfkicthen_saT4uf1FX.png", "https://ik.imagekit.io/toto55/9mins3_BSMcP2wcF.png", "https://ik.imagekit.io/toto55/9mins5_Wns5vQAQqD.png", "https://ik.imagekit.io/toto55/9mins6_b1H0k-oR6.png"],
-      "price": "130,000 JPY per month (23.4 sqm, all bills inc.)",
+      "price": "120,000 JPY per month (23.4 sqm, all bills inc.)",
       "amenities": "Studio Apt., Double bed, Small Kitchen, TV, Sofa, Laundry in Building, All Bills inc., Portable Wi-Fi, Concrete Walls, Double-glazed Windows",
       "location": "Higashi Shinjuku - 5 mins, Shinjuku Sanchome - 8 mins, JR Shinjuku - 14 mins",
-      "availability": "From January 5th 2020"
+      "availability": "From end of March 2020"
     },
-    {
-      "name": "9 mins to Shinjuku",
-      "description": "Next to the amazing Shinjuku Gyoen park!",
-      "descriptionLong": "",
-      "mainImage": "https://ik.imagekit.io/toto55/aptOne1_QPUOVnZ77.png",
-      "images": ["https://ik.imagekit.io/toto55/aptOne1_QPUOVnZ77.png","https://ik.imagekit.io/toto55/9all2_9gRctejnl.png", "https://ik.imagekit.io/toto55/aptOne2_pl-3OCd1U.png", "https://ik.imagekit.io/toto55/9tiolet_u4VjSMjw4.png", "https://ik.imagekit.io/toto55/aptOne4_Qn9FiVIQe.png", "https://ik.imagekit.io/toto55/aptOne5_9kncIsPJQ.png", "https://ik.imagekit.io/toto55/aptOne6_87zYi8LW3.png"],
-      "price": "140,000 JPY per month (27.8 sqm, all bills inc.)",
-      "amenities": "Studio Apt., Double bed, Kitchen, TV, Sofa, Washing Machine, All Bills inc., High-Speed Unlimited Wi-Fi, Concrete Walls, Double-glazed Windows",
-      "location": "Shinjuku Gyoen Mae - 3 mins, Shinjuku Sanchome - 5 mins, JR Shinjuku - 9 mins",
-      "availability": "From January 5th 2020"
-
-    }
+    
   ]
